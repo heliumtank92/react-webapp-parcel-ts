743ce84a3220e78a1188c813b9ee04c1fd8e2f33
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-06-29 14:25:39 +0530

    Version File Support added

--- ejs.config.js
+++ ejs.config.js
@@ -1,9 +1,18 @@
+const packageJSON = require('./package.json')
 require('custom-env').env(process.env.NODE_ENV)
+const { name, description, version } = packageJSON
 
 const envConfig = {
   ...process.env,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  BABEL_ENV: process.env.BABEL_ENV || 'development'
+  BABEL_ENV: process.env.BABEL_ENV || 'development',
+
+  // For version.json file
+  name,
+  description,
+  version,
+  buildAt: new Date(),
+  buildEnv: process.env.NODE_ENV || 'development'
 }
 
 const config = { locals: envConfig }
