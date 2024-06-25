90170967a3e1a1d866bc8e8942ea79aaf3ebfc0c
Mehul Agarwal <agarwalmehul92@gmail.com>
2024-06-25 23:23:44 +0530

    Add defaultMode var in Theme Reducer

--- src/Redux/Theme/TYPES.ts
+++ src/Redux/Theme/TYPES.ts
@@ -1,12 +1,14 @@
 import { DsPalette, SupportedColorScheme } from '@am92/react-design-system'
 
 export type T_THEME_REDUCER = {
+  defaultMode: 'light' | 'dark' | 'system'
   scheme: SupportedColorScheme
   palette: DsPalette
   fontFamily: string
 }
 
 export const DEFAULT_THEME: T_THEME_REDUCER = {
+  defaultMode: 'light',
   scheme: 'light',
   palette: {},
   fontFamily: 'Lato'
